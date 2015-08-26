a8e9bfb269e9a2af45ab2bb4dcbd48e05a50195a
Benoit Letondor <benoit.letondor@gmail.com>
2015-08-26 18:15:56 +0200

    Add bug report option

--- Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudget/view/PreferencesFragment.java
+++ Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudget/view/PreferencesFragment.java
@@ -1,12 +1,16 @@
 package com.benoitletondor.easybudget.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.benoitletondor.easybudget.R;
+import com.benoitletondor.easybudget.helper.ParameterKeys;
+import com.benoitletondor.easybudget.helper.Parameters;
 
 /**
  * Fragment to display preferences
@@ -22,6 +26,27 @@ public class PreferencesFragment extends PreferenceFragment
 
         // Load the preferences from the XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        /*
+         * Bind bug report button
+         */
+        findPreference(getResources().getString(R.string.setting_category_bug_report_send_button_key)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
+        {
+            @Override
+            public boolean onPreferenceClick(Preference preference)
+            {
+                String localId = Parameters.getInstance(getActivity()).getString(ParameterKeys.LOCAL_ID);
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.setting_category_bug_report_send_text)+localId);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+                return false;
+            }
+        });
+
     }
 
 
