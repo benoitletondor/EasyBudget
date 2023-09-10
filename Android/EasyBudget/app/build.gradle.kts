ad3b48b8a5f9d97dfe114ba07862048127898624
Benoit Letondor <benoit.letondor@gmail.com>
2023-09-10 14:43:46 +0200

    Bump version to 3.0.3

--- Android/EasyBudget/app/build.gradle.kts
+++ Android/EasyBudget/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         compileSdk = 33
         minSdk = 21
         targetSdk = 33
-        versionCode = 101
-        versionName = "3.0.2"
+        versionCode = 102
+        versionName = "3.0.3"
         vectorDrawables.useSupportLibrary = true
 
         javaCompileOptions {
