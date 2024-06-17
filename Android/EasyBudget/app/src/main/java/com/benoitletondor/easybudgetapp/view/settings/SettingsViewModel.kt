89ebfa0df340c51403220a00cff6d874510d3d0b
Benoit Letondor <benoit.letondor@gmail.com>
2024-06-17 23:38:02 +0200

    Bump deps

--- Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/view/settings/SettingsViewModel.kt
+++ Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/view/settings/SettingsViewModel.kt
@@ -3,11 +3,11 @@ package com.benoitletondor.easybudgetapp.view.settings
 import androidx.lifecycle.ViewModel
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
 @HiltViewModel(assistedFactory = SettingsViewModelFactory::class)
-class SettingsViewModel @Inject constructor(
+class SettingsViewModel @AssistedInject constructor(
     @Assisted private val redirectToBackupSettings: Boolean,
 ) : ViewModel() {
 
