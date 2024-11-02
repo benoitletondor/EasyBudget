a1ff2b039daf597cf4d8f8e8af58f66ab02e15cb
Benoit Letondor <benoit.letondor@gmail.com>
2024-11-02 14:54:21 +0100

    Add app update manager

--- Android/EasyBudget/app/build.gradle.kts
+++ Android/EasyBudget/app/build.gradle.kts
@@ -125,6 +125,7 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.10.0")
     implementation("androidx.work:work-gcm:2.10.0")
     implementation("com.google.android.play:review-ktx:2.0.2")
+    implementation("com.google.android.play:app-update-ktx:2.1.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
--- Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/injection/AppModule.kt
+++ Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/injection/AppModule.kt
@@ -17,6 +17,7 @@
 package com.benoitletondor.easybudgetapp.injection
 
 import android.content.Context
+import com.benoitletondor.easybudgetapp.AppUpdateManager
 import com.benoitletondor.easybudgetapp.BuildConfig
 import com.benoitletondor.easybudgetapp.accounts.Accounts
 import com.benoitletondor.easybudgetapp.accounts.FirebaseAccounts
@@ -88,6 +89,12 @@ object AppModule {
     @Provides
     @Singleton
     fun provideConfig(): Config = FirebaseRemoteConfig()
+
+    @Provides
+    @Singleton
+    fun provideAppUpdateManager(
+        @ApplicationContext context: Context,
+    ) = AppUpdateManager(context)
 
     private var app: App? = null
     private var usedOnlineDB: CachedOnlineDBImpl? = null
