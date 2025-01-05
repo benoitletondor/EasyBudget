8717065d7daac2c1d0f95910e26b3dad43ecce63
Benoit Letondor <benoit.letondor@gmail.com>
2025-01-05 15:52:59 +0100

    Fix wrong sign on Monthly report

--- Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/view/monthlyreport/subviews/Entry.kt
+++ Android/EasyBudget/app/src/main/java/com/benoitletondor/easybudgetapp/view/monthlyreport/subviews/Entry.kt
@@ -98,7 +98,7 @@ fun Entry(
                 )
 
                 Text(
-                    text = CurrencyHelper.getFormattedCurrencyString(currency, expense.amount),
+                    text = CurrencyHelper.getFormattedCurrencyString(currency, -expense.amount),
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
                     color = colorResource(if (expense.isRevenue()) R.color.budget_green else R.color.budget_red),
